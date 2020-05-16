1d3861e04dee14ef02d2edd5a718526cdcdbddd1
Durgesh <durgesh123.iitr@gmail.com>
2020-05-17 00:04:20 +0530

    some changes in action file

--- lib/features/home/presentation/screens/home.dart
+++ lib/features/home/presentation/screens/home.dart
@@ -13,7 +13,6 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
